5e5fcee3638fc10457e0ad910b701143fb2783f6
root <root@localhost.localdomain>
2015-10-21 06:28:24 -0500

    Modify folmders

--- dork-and-diff.sh
+++ dork-and-diff.sh
@@ -1,49 +1,61 @@
 #!/bin/bash
 
 site=.com
-filename=dorks.txt
-results=`date "+%Y%m%d-%H%M%S"`-results.txt
-echo $results
-i=0
 tempfile=temp.log
+filename=dorks/dorks.txt
 WAIT=5
+mkdir output
+
+if [ -z "$1" ]
+then
+        echo "No argument 1 supplied (temp file)"
+else
+        # TODO - do not run as root
+                tempfile=$1
+fi
+
+if [ -z "$2" ]
+then
+        echo "No argument 2 supplied (site: dork)"
+else
+        # TODO - do not run as root
+        site=$2
+fi
+
+if [ -z "$3" ]
+then
+        echo "No argument 3 supplied (dork list, taking dorks.txt)"
+else
+        # TODO - do not run as root
+        filename=$3
+fi
+
+if [ -z "$4" ]
+then
+        echo "No argument 4 supplied (wait time)"
+else
+        # TODO - do not run as root
+        WAIT=$4
+fi
+
 
 while read -r line
 do
         dork="site:$site $line"
-	echo " " >> $tempfile
-	
-	echo "[INFO] - Dorking:$dork"
-	echo "** Dorking: $dork ----------------------------------" >> $tempfile		
-	curl -A "Mozilla/Firefox" -s --get --data-urlencode "q=$dork" http://ajax.googleapis.com/ajax/services/search/web?v=1.0 | egrep -o "http[^\"]+" | egrep -v "(www.youtube.com)|(google.com)|(/search\?q=cache)" >> $tempfile 
-	
-	# echo "$LINKS" | cut -d / -f 3 | cut -d : -f 1 | grep -v "^$"
 
-	echo "[INFO] - Sleeping to avoid lockout"
-	sleep $WAIT
-	
+        echo "[INFO] - Dorking:$dork"
+        echo "" >> $tempfile
+        #echo "" >> $tempfile
+        echo "-----------------------" >> $tempfile
+        echo "   $dork               " >> $tempfile
+        echo "-----------------------" >> $tempfile
+
+        curl -A "Mozilla/Firefox" -s --get --data-urlencode "q=$dork" http://ajax.googleapis.com/ajax/services/search/web?v=1.0 | egrep -o "http[^\"]+" | egrep -v "(www.youtube.com)|(google.com)|(/search\?q=cache)" >> $tempfile
+
+        echo "" >> $tempfile
+        # echo "$LINKS" | cut -d / -f 3 | cut -d : -f 1 | grep -v "^$"
+        echo "[INFO] - Sleeping to avoid lockout"
+        sleep $WAIT
+
 done < "$filename"
-
-# remove duplicates from file
-awk '!seen[$0]++' $tempfile > $results
-
-rm -f $tempfile
-cat $results;
-
-# if do not exist, create old and latest
-mkdir archive
-mkdir latest
-mkdir diffed
-
-echo " -----------------  DIFF WITH LATEST -----------------"
-mv diffed/*.* archive/
-diff $results latest/*.txt > diffed/$results-diff-with-latest.txt
-mv latest/*.txt archive/
-mv $results latest/
-
-
-cat diffed/$results-diff-with-latest.txt
-
-
-
 
